4bd1df914725218e16a84f68cc5896f290ddc6fc
NguyenNhatAnh <Nguyennhatanhck2003@Gmail.com>
2025-03-05 14:41:58 +0700

    ChiTietNhanVien

--- src/main/java/com/example/qtdl/HelloApplication.java
+++ src/main/java/com/example/qtdl/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("ChiTietNV.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 520, 400);
         stage.setScene(scene);
         stage.setResizable(false);
