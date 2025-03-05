7d50146a6d8338beabecc0bea0257936d42ca57f
HUYNH PHUOC HUNG B2205941 <hungb2205941@student.ctu.edu.vn>
2025-03-05 14:45:31 +0700

    aaaa

--- src/main/java/com/example/qtdl/HelloApplication.java
+++ src/main/java/com/example/qtdl/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 520, 400);
+        Scene scene = new Scene(fxmlLoader.load(), 520, 406);
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
